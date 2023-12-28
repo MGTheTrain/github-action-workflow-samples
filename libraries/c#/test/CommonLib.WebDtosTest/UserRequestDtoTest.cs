ba7628e9e920873f3397abf6e962fc777c6f92fd
Marvin Gajek <Marvin.Gajek1@web.de>
2023-12-28 14:24:18 +0100

    update test for web dtos

--- libraries/c#/test/CommonLib.WebDtosTest/UserRequestDtoTest.cs
+++ libraries/c#/test/CommonLib.WebDtosTest/UserRequestDtoTest.cs
@@ -1,10 +1,42 @@
-namespace CommonLib.WebDtosTest;
+using CommonLib.Web.Dtos;
 
-public class UnitTest1
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace CommonLib.WebDtosTest
 {
-    [Fact]
-    public void Test1()
+    public class UserRequestDtosTests
     {
+        [Fact]
+        public void Validate_UserRequestDtoProperties_ReturnsExpectedValidationResults()
+        {
+            // Arrange
+            var userRequestDto = new UserRequestDto(); // Create an instance of the User class
 
-    }
+            // Set other properties for testing
+            userRequestDto.userName = "ValidUserName";
+            userRequestDto.userPassword = "StrongPassword123!";
+            userRequestDto.email = "test@example.com";
+            userRequestDto.userId = Guid.NewGuid();
+            userRequestDto.dateTimeCreated = DateTime.Now;
+            userRequestDto.dateTimeUpdated = DateTime.Now;
+
+            // Additional null checks
+            Assert.NotNull(userRequestDto.userId);
+            Assert.NotNull(userRequestDto.dateTimeCreated);
+            Assert.NotNull(userRequestDto.dateTimeUpdated);
+            Assert.NotNull(userRequestDto.userName);
+            Assert.NotNull(userRequestDto.userPassword);
+            Assert.NotNull(userRequestDto.email);
+
+            // Act
+            var validationContext = new ValidationContext(userRequestDto);
+            var validationResults = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(userRequestDto, validationContext, validationResults, true);
+
+            // Assert
+            Assert.True(isValid); // Ensure validation passes
+            Assert.Empty(validationResults); // Ensure no validation errors
+        }
+    }    
 }
