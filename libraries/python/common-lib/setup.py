a0d55ca78418bcd5eb9c8cd0e3b12d82a1d7194e
Marvin Gajek <Marvin.Gajek1@web.de>
2023-12-29 00:22:28 +0100

    consider version input argument

--- libraries/python/common-lib/setup.py
+++ libraries/python/common-lib/setup.py
@@ -1,8 +1,19 @@
 from setuptools import setup, find_packages
+import sys
+
+# Default version
+version = '0.1.0'
+
+if '--version' in sys.argv:
+    version_index = sys.argv.index('--version') + 1
+    if version_index < len(sys.argv):
+        version = sys.argv[version_index]
+        del sys.argv[version_index]
+        del sys.argv[version_index - 1]
 
 setup(
     name='common-lib',
-    version='0.1.0',
+    version=version,
     description='Common logic utilized in several services and applications',
     author='MGTheTrain',
     author_email='mgthetrain@email.com',
