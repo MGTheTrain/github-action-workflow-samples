d2057fffb252b64dbaea8ffa4755fb7786bedfe7
Marvin Gajek <Marvin.Gajek1@web.de>
2023-12-29 15:00:48 +0100

    update comment

---
+++ include-package-samples/python/utilize-common-lib-app.py
@@ -0,0 +1,4 @@
+
+
+# To leverage the functionalities offered by the CommonLib .NET package, access the 'Setup' view within the NuGet package and perform the following steps:
+# pip install --index-url=<> common-lib==0.1.0.dev9
