9517ad74bcc434acd8137160aef37a342ca234cb
Marvin Gajek <Marvin.Gajek1@web.de>
2023-12-29 14:50:54 +0100

    add sample repository in which to utilize the dotnet package residing in the private CloudSmith repo

---
+++ include-package-samples/c#/UtilizeCommonLibApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CommonLib.Domain.Models;
+
+class Program
+{
+    static void Main()
+    {
+        var user = new User
+        {
+            userName = "ValidUserName",
+            userPassword = "StrongPassword123!",
+            email = "test@example.com",
+            userId = Guid.NewGuid(),
+            dateTimeCreated = DateTime.Now,
+            dateTimeUpdated = DateTime.Now
+        };
+
+        PrintUserAttributes(user);
+    }
+
+    static void PrintUserAttributes(User user)
+    {
+        Console.WriteLine("User Attributes:");
+        Console.WriteLine($"User Name: {user.userName}");
+        Console.WriteLine($"User Password: {user.userPassword}");
+        Console.WriteLine($"Email: {user.email}");
+        Console.WriteLine($"User ID: {user.userId}");
+        Console.WriteLine($"Date Time Created: {user.dateTimeCreated}");
+        Console.WriteLine($"Date Time Updated: {user.dateTimeUpdated}");
+    }
+}
