e5ba358afa3248b89fdd204fe5b7d83e8925a245
Marvin Gajek <Marvin.Gajek1@web.de>
2023-12-29 15:06:55 +0100

    update comment

--- include-package-samples/python/utilize-common-lib-app.py
+++ include-package-samples/python/utilize-common-lib-app.py
@@ -1,4 +1,4 @@
-# To leverage the functionalities offered by the common-lib pip package, access the 'Setup' view within the NuGet package and perform the following steps:
+# To leverage the functionalities offered by the common-lib pip package, access the 'Setup' view within the pip package and perform the following steps:
 # pip install --index-url=<> common-lib==0.1.0.dev9
 
 from datetime import datetime
