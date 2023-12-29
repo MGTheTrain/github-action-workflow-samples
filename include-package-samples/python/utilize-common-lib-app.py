934bd860c6e2cd340c62b49d6ccec40f7b5941c4
Marvin Gajek <Marvin.Gajek1@web.de>
2023-12-29 15:05:37 +0100

    add sample in which to utilize pip package residing in the private CloudSmith repo

--- include-package-samples/python/utilize-common-lib-app.py
+++ include-package-samples/python/utilize-common-lib-app.py
@@ -1,4 +1,27 @@
-
-
-# To leverage the functionalities offered by the CommonLib .NET package, access the 'Setup' view within the NuGet package and perform the following steps:
+# To leverage the functionalities offered by the common-lib pip package, access the 'Setup' view within the NuGet package and perform the following steps:
 # pip install --index-url=<> common-lib==0.1.0.dev9
+
+from datetime import datetime
+from domain.models.user import User
+
+def valid_user():
+    return User(
+        userId="123",
+        userName="ValidUserName123",
+        userPassword="StrongPassword123!?§$%",
+        email="test@example.com",
+        dateTimeCreated=datetime.now(),
+        dateTimeUpdated=datetime.now()
+    )
+
+def log_user_attributes(user):
+    print("User Attributes:")
+    print(f"User ID: {user.userId}")
+    print(f"User Name: {user.userName}")
+    print(f"User Password: {user.userPassword}")
+    print(f"Email: {user.email}")
+    print(f"Date Time Created: {user.dateTimeCreated}")
+    print(f"Date Time Updated: {user.dateTimeUpdated}")
+
+user = valid_user()
+log_user_attributes(user)
